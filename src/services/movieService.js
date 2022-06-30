1c486928ccf257bbc7b34c448f1cd5a29e98b1fd
Asmaa Moallim <41068564+AsmaaMoallim@users.noreply.github.com>
2022-06-30 12:39:20 +0300

    first commit

---
+++ src/services/movieService.js
@@ -0,0 +1,26 @@
+// import * as genresAPI from "./genreService";
+
+// const movie = [
+//   {
+//     _id: "1",
+//     title: "Die Hard",
+//     genre: {
+//       _id: "1",
+//       name: "Action",
+//     },
+//     numberInStock: 5,
+//     dailyRentalRate: 2.5,
+//     publishDate: "2018-01-03T19:28.8092",
+//   },
+//   {
+//     _id: "2",
+//     title: "Cold sea",
+//     genre: {
+//       _id: "2",
+//       name: "Horror",
+//     },
+//     numberInStock: 7,
+//     dailyRentalRate: 4,
+//     publishDate: "2008-01-03T19:28.8092",
+//   },
+// ];
